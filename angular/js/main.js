c842f893d6381f9727951dea547d10643b80ad32
huhuaijin <1159279056@qq.com>
2016-06-17 11:12:01 +0800

    提交angular所有内容

---
+++ angular/js/main.js
@@ -0,0 +1,10 @@
+﻿/**
+ * Created by Administrator on 2016/6/17.
+ */
+app.controller('btTable',function($scope){
+    $scope.firstname='huhuaijin';
+    $scope.lastname="huhuhu";
+    $scope.names = [{edit:'abc1',firstname:'张三',lastname:'张小三'},{edit:'bcd2',firstname:'李四',lastname:'李小四'},{edit:'bcd3',firstname:'王五',lastname:'王小五'},{edit:'bcd4',firstname:'小白',lastname:'小小白'}]
+    $scope.error = false;
+    $scope.incomplete = false;
+});
